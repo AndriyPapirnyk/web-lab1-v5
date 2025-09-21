8f09b2ae53605ea7907a920f5e2115612e353500
AndriyPapirnyk <papirnykandriy@gmail.com>
2025-09-21 23:24:09 +0300

    burger menu fuctionality

--- javascript/script.js
+++ javascript/script.js
@@ -2,13 +2,5 @@ const burger = document.getElementById('header__burger');
 const nav = document.getElementById('header__nav');
 
 burger.addEventListener('click', () => {
-    console.log('click');
-    console.log(nav.style.display);
-    if(nav.style.display === 'none') {
-        nav.style.display = 'block';
-        console.log(1)
-    }else {
-        nav.style.display = 'block';
-        console.log(2)
-    }
-})
+  nav.classList.toggle('active');
+});
